2e6ecdb8210214cacc4c7dfe3bab8dcd94525d01
wislon <wislon@gmail.com>
2014-07-24 13:52:45 +1000

    Added constants corresponding to the Android.BluetoothAdapter state

---
+++ src/BluetoothWidget/AppConstants.cs
@@ -0,0 +1,10 @@
+namespace BluetoothWidget
+{
+  public class AppConstants
+  {
+    public const int STATE_OFF = 10;
+    public const int STATE_TURNING_ON = 11;
+    public const int STATE_ON = 12;
+    public const int STATE_TURNING_OFF = 13;
+  }
+}
