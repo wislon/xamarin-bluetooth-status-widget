ba37d74ac934b1cbf83133628b754c8608376cac
wislon <wislon@gmail.com>
2014-07-29 20:16:44 +1000

    auto-regenerated resource stuff

--- src/BluetoothWidget/Resources/Resource.Designer.cs
+++ src/BluetoothWidget/Resources/Resource.Designer.cs
@@ -45,16 +45,13 @@ namespace BluetoothWidget
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int bluetooth_file_exchange = 2130837504;
+			public const int bluetooth_off = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int bluetooth_off = 2130837505;
+			public const int bluetooth_on = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int bluetooth_on = 2130837506;
-			
-			// aapt resource value: 0x7f020003
-			public const int icon = 2130837507;
+			public const int icon = 2130837506;
 			
 			static Drawable()
 			{
